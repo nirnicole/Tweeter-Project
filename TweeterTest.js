26466eaa2ad20886f92c4321217c6a62dc78d1d0
nir nicole <niko426@gmail.com>
2022-08-24 00:52:17 +0300

    Update TweeterTest.js

--- TweeterTest.js
+++ TweeterTest.js
@@ -1,28 +1,7 @@
 const tweeter = TweeterLogic();
 const tweeterRendering = TweeterRender();
 
-tweeter.addPost("This is my own post!");
-tweeter.addPost("2 post");
-tweeter.addPost("3 post");
-tweeter.addPost("My second post");
-tweeter.addPost("Become A New York Times Subscriber Today!");
-console.log(tweeter.getPosts());
 
-tweeter.removePost("p2");
-tweeter.removePost("p3");
-console.log(tweeter.getPosts());
-
-tweeter.addComment("p1", "im a comment");
-tweeter.addComment("p4", `im a comment..`);
-tweeter.addComment("p5", "eiiiii");
-tweeter.addComment("p5", "eiiiii");
-tweeter.addComment("p5", "blabla");
-tweeter.addComment("p5", "a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text a lot of text ");
-tweeter.addComment("p5", "dont take me down!");
-console.log(tweeter.getPosts());
-
-tweeter.removeComment("p1", "c2");
-console.log(tweeter.getPosts());
 
 
 tweeterRendering.renderPosts(tweeter.getPosts())
@@ -33,9 +12,12 @@ tweeterRendering.renderPosts(tweeter.getPosts())
 //add post
 $('body').on('click', '#postbtn', function() {
     let postText = $("#input").val()
-    console.log(postText)
-    tweeter.addPost(postText);
+    $("#input").val('')
+
+    if(postText!==""){
+    tweeter.addPost(postText)
     tweeterRendering.renderPosts(tweeter.getPosts())
+    }
   })
   
 //delete comment
@@ -50,7 +32,6 @@ $('#posts').on('click','.delete-comment', function() {
 //delete post
 $('#posts').on('click','.delete-post', function() {
     let postId = $(this).closest(".post").attr("id")
-    console.log(postId)
     tweeter.removePost(postId)
     tweeterRendering.renderPosts(tweeter.getPosts())
   })
@@ -60,7 +41,8 @@ $('#posts').on('click','.delete-post', function() {
 $('#posts').on('click','.comment-btn', function() {
     const commentText = $(this).siblings(".comment-input").val()
     const postId = $(this).closest(".post").attr("id")
-
+    if(commentText!==""){
     tweeter.addComment(postId, commentText)
     tweeterRendering.renderPosts(tweeter.getPosts())
+    }
   })
